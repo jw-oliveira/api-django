28ca8304265838bbb8e9a567141ec40dbffc6da1
Jorge Willian de Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2023-11-16 19:50:49 -0300

    feat-add-post-method
    Adicionado método post de teste

--- primeiro_projeto/app_teste/views.py
+++ primeiro_projeto/app_teste/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
 def index(resquest):
     return HttpResponse('Hello World!')
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
